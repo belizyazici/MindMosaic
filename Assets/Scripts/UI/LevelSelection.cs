137f533a91e3acd9f0395b1b01fc10c1fae17faa
Beliz Yazıcı <beliz.yazici@std.izmirekonomi.edu.tr>
2024-02-09 02:26:28 +0300

    fourth levels item and obstacles are located

--- Assets/Scripts/UI/LevelSelection.cs
+++ Assets/Scripts/UI/LevelSelection.cs
@@ -33,7 +33,7 @@ public class LevelSelection : MonoBehaviour
 
         Button f_btn = forwardButton.GetComponent<Button>();
         Button b_btn = backwardsButton.GetComponent<Button>();
-
+        
         int levelAt = PlayerPrefs.GetInt("levelAt", 2);
 
         for (int i = 0; i < levelButtons.Length; i++)
@@ -41,26 +41,63 @@ public class LevelSelection : MonoBehaviour
             if (i + 2 > levelAt)
             {
                 levelButtons[i].interactable = false;
+                button1.onClick.AddListener(GoToThatLevel);
+                button2.onClick.AddListener(GoToThatLevel);
+                button3.onClick.AddListener(GoToThatLevel);
+                button4.onClick.AddListener(GoToThatLevel);
+                button5.onClick.AddListener(GoToThatLevel);
+                button6.onClick.AddListener(GoToThatLevel);
+                button7.onClick.AddListener(GoToThatLevel);
+                button8.onClick.AddListener(GoToThatLevel);
+                button9.onClick.AddListener(GoToThatLevel);
+
+                f_btn.onClick.AddListener(GoBackOrForth);
+                b_btn.onClick.AddListener(GoBackOrForth);
             }
         }
         
 
-        button1.onClick.AddListener(GoToThatLevel);
-        button2.onClick.AddListener(GoToThatLevel);
-        button3.onClick.AddListener(GoToThatLevel);
-        button4.onClick.AddListener(GoToThatLevel);
-        button5.onClick.AddListener(GoToThatLevel);
-        button6.onClick.AddListener(GoToThatLevel);
-        button7.onClick.AddListener(GoToThatLevel);
-        button8.onClick.AddListener(GoToThatLevel);
-        button9.onClick.AddListener(GoToThatLevel);
-
-        f_btn.onClick.AddListener(GoBackOrForth);
-        b_btn.onClick.AddListener(GoBackOrForth);
+        
     }
 
     public void GoToThatLevel()
     {
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.CompareTag("Level1"))
+        {
+            SceneManager.LoadScene(2);
+        }
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.CompareTag("Level2"))
+        {
+            SceneManager.LoadScene(3);
+        }
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.CompareTag("Level3"))
+        {
+            SceneManager.LoadScene(4);
+        }
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.CompareTag("Level4"))
+        {
+            SceneManager.LoadScene(5);
+        }
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.CompareTag("Level5"))
+        {
+            SceneManager.LoadScene(6);
+        }
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.CompareTag("Level6"))
+        {
+            SceneManager.LoadScene(7);
+        }
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.CompareTag("Level7"))
+        {
+            SceneManager.LoadScene(8);
+        }
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.CompareTag("Level8"))
+        {
+            SceneManager.LoadScene(9);
+        }
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.CompareTag("Level9"))
+        {
+            SceneManager.LoadScene(10);
+        }
         
     }
 
