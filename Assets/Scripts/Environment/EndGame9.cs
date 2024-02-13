7f4034e07283f30477b9150a0581ed99d1f1c2d8
Beliz Yazıcı <beliz.yazici@std.izmirekonomi.edu.tr>
2024-02-13 21:20:21 +0300

    Level9 is completed

---
+++ Assets/Scripts/Environment/EndGame9.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class EndGame9 : MonoBehaviour
+{
+    public GameObject victoryPanel;
+    public GameObject gameOverEndPanel;
+    
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<Player>();
+        ItemCounter itemCounter = other.GetComponent<ItemCounter>();
+        
+        if (player != null)
+        {
+            if (itemCounter != null && itemCounter.NumberOfItems >= 130)
+            {
+                victoryPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+        
+            else
+            {
+                gameOverEndPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+        
+        
+    }
+}
